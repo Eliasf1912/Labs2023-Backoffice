3126863e69a3381e228dcd3468c23559cb8f2c34
Elias Duvernois <e.duvernois2016@gmail.com>
2023-03-10 13:47:03 +0100

    initizilisation du github

---
+++ src/conponents/Nav.js
@@ -0,0 +1,16 @@
+import './Nav.css'
+
+const Nav = () => {
+    return(
+        <div className="Nav">
+            <nav>
+               <ul>
+                    <li>Changer les tracklist</li>
+                    <li>Changer les dates de tourner</li>
+               </ul> 
+            </nav>
+        </div>
+    );
+}
+ 
+export default Nav;
